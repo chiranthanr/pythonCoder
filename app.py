d2d9e15830e4a153f47d7978683cf7f60542cc8b
Chiranthan Ram <chiranthan@gmail.com>
2024-04-14 15:53:42 +0530

    Python coding assistant project using Langchain and Ollama with a locally installed Mistral 7B model

---
+++ app.py
@@ -0,0 +1,39 @@
+from langchain_community.llms import Ollama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import Runnable
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable.config import RunnableConfig
+
+import chainlit as cl
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    model = Ollama(model="pythonCoder")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are an expert Python programmer who is proficient in all aspects of the python language. You will "
+                "act as an assistant to help with writing code based on any use case given to you."
+            ),
+            ("human","{question}")
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable",runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+            {"question": message.content},
+            config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()])
+    ):
+        await msg.stream_token(chunk)
+    await msg.send()
+
---
+++ chainlit.md
@@ -0,0 +1,14 @@
+# Python programming companion App 🐍
+### This is a friendly chat interface built using Mistral's foundational model to help the user with queries on syntax and functions of Python programming language.
+
+#### To use this application, following these steps:
+
+* Clone the Github repository *https://github.com/chiranthanr/pythonCoder.git*
+* Install python 3.11 or later in your local system
+* Install Ollama in your local computer with instructions in this link - https://github.com/ollama/ollama/blob/main/README.md
+* Install all required libraries in your environment using '> pip install -r requirements.txt'
+* Run Ollama in your local machine with the command '> ollama serve'.If the service is already running then you will 
+  be notified accordingly
+* Run the pythonCoder application using '> chainlit run app.py'
+
+#### This will now open a chat interface where you can write your python related queries and get instant responses.
